4d26f6a0b64417af5de2ab53cd89aa1084a24cb4
Aniket Kotal <aniket1010104@gmail.com>
2023-10-23 01:54:56 +0530

    added .env typings

---
+++ env.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      PORT: string;
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_NAME: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+
+      DATABASE_URL: "postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=public";
+    }
+  }
+}
+
+export {}
